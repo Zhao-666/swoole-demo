675bb2cca88c0c22f7defa371e470097ea185221
Zhao-666 <2268699370@qq.com>
2018-09-14 16:50:41 +0800

    Task机制 demo

--- demo/server/Ws.php
+++ demo/server/Ws.php
@@ -18,15 +18,31 @@ class Ws
         $this->server = new swoole_websocket_server(self::HOST, self::PORT);
 
         $this->server->set([
+            'task_worker_num' => 2,
             'enable_static_handler' => true,
             'document_root' => '/mnt/htdocs/swoole-demo/data',
         ]);
 
         $this->server->on('open', [$this, 'onOpen']);
         $this->server->on('message', [$this, 'onMessage']);
+        $this->server->on('task', [$this, 'onTask']);
+        $this->server->on('finish', [$this, 'onFinish']);
         $this->server->on('close', [$this, 'onClose']);
 
         $this->server->start();
+    }
+
+    public function onTask($server, $taskId, $workerId, $data)
+    {
+        print_r($data);
+        sleep(5);
+        return 'on task finish';
+    }
+
+    public function onFinish($server, $taskId, $data)
+    {
+        echo 'taskId: ' . $taskId . PHP_EOL;
+        echo 'finish data success ' . $data . PHP_EOL;
     }
 
     /**
@@ -47,6 +63,11 @@ class Ws
     public function onMessage($server, $frame)
     {
         echo 'ser-push-message:' . $frame->data . "\n";
+        $data = [
+            'task' => 1,
+            'fd' => $frame->fd
+        ];
+        $server->task($data);
         $server->push($frame->fd, 'server-push:' . date('Y-m-d H:i:s'));
     }
 
