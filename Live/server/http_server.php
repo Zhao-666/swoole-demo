47b10182cfbb7d1b27b3f0c2fbc986639324a743
Zhao-666 <2268699370@qq.com>
2018-09-26 23:05:27 +0800

    init tp5

--- Live/server/http_server.php
+++ Live/server/http_server.php
@@ -14,8 +14,48 @@ $http->set([
     'document_root' => '/mnt/htdocs/swoole-demo/Live/public/static',
 ]);
 
-$http->on('request', function ($request, $response) {
-    $response->end(json_encode($request->header));
+$http->on('WorkerStart', function (swoole_http_server $server, $workerId) {
+    echo 'worker_start-' . $workerId . PHP_EOL;
+
+    // 加载基础文件
+    require __DIR__ . '/../thinkphp/base.php';
+
+});
+
+$http->on('request', function ($request, $response) use ($http) {
+//    $_SERVER = [];
+    if (isset($request->server)) {
+        foreach ($request->server as $k => $v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    if (isset($request->header)) {
+        foreach ($request->header as $k => $v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    $_GET = [];
+    if (isset($request->get)) {
+        foreach ($request->get as $k => $v) {
+            $_GET[$k] = $v;
+        }
+    }
+    $_POST = [];
+    if (isset($request->post)) {
+        foreach ($request->post as $k => $v) {
+            $_POST[$k] = $v;
+        }
+    }
+    ob_start();
+    try {
+        \think\Container::get('app')->run()->send();
+    } catch (Exception $e) {
+    }
+//    echo 'action-' . request()->action() . PHP_EOL;
+    $ret = ob_get_contents();
+    ob_end_clean();
+    $response->end($ret);
+//    $http->close();
 });
 
 $http->start();
